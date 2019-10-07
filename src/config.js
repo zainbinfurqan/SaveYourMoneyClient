e0364d0670a6e38bda8a405deacf1f4c928bd386
zain ahmed <zain.ahmed199524@gmail.com>
2019-10-07 15:55:35 +0500

    v

--- src/config.js
+++ src/config.js
@@ -1,18 +1,18 @@
 let Config = {
     // PERSIST_SECRET_KEY: "",
-    ENVIRONMENT: "LOCAL",
+    // ENVIRONMENT: "LOCAL",
     ENVIRONMENTS: {
-      LOCAL: {
-        // API_URL: "http://localhost:4000"
-        API_URL: "http://localhost:3000"
-        // API_URL: "https://savemoneyserver.herokuapp.com"
-        // API_URL: "https://desolate-stream-27036.herokuapp.com"
-        // BASE_IMG_PATH: ""
-      }
-      // DEVELOPMENT: {
-      //     API_URL: "",
-      //     BASE_IMG_PATH: ""
-      // },
+    //   LOCAL: {
+    //     // API_URL: "http://localhost:4000"
+    //     API_URL: "http://localhost:3000"
+    //     // API_URL: "https://savemoneyserver.herokuapp.com"
+    //     // API_URL: "https://desolate-stream-27036.herokuapp.com"
+    //     // BASE_IMG_PATH: ""
+    //   }
+      DEVELOPMENT: {
+          API_URL: "https://shrouded-savannah-54835.herokuapp.com",
+          // BASE_IMG_PATH: ""
+      },
       // PRODUCTION: {
       //     API_URL: "",
       //     BASE_IMG_PATH: ""
