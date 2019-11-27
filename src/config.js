da440f608f65c46fa9ec414b243cdb04c32a4b71
zainbinfurqan <zain.ahmed199524@gmail.com>
2019-11-28 01:07:15 +0500

    11/28/2019

--- src/config.js
+++ src/config.js
@@ -1,17 +1,17 @@
 let Config = {
     // PERSIST_SECRET_KEY: "",
     // ENVIRONMENT: "LOCAL",
-    ENVIRONMENT: "LOCAL",
+    ENVIRONMENT: "DEVELOPMENT",
     ENVIRONMENTS: {
       LOCAL: {
-        API_URL: "http://localhost:3001"
+        // API_URL: "http://localhost:3001"
         // API_URL: "http://localhost:3000"
         // API_URL: "https://shrouded-savannah-54835.herokuapp.com/"
     //     // API_URL: "https://desolate-stream-27036.herokuapp.com"
     //     // BASE_IMG_PATH: ""
       },
       DEVELOPMENT: {
-          // API_URL: "https://shrouded-savannah-54835.herokuapp.com",
+          API_URL: "https://shrouded-savannah-54835.herokuapp.com",
           // BASE_IMG_PATH: ""
       },
       // PRODUCTION: {
